75515d1457662235652e248937f204dbbeff4a68
balu balaji <balajipuvvada12289@example.com>
2020-04-20 12:17:56 +0530

    with one on one chat

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express=require('express')
 const session=require('express-session')
 const app=express()
 const register=require('./server/register')
+const profile=require('./server/profile')
 const login=require('./server/login')
 const logout=require('./server/logout')
+const friend=require('./server/friends')
+const messages=require('./server/messanger')
 const path=require('path')
+const suggestions=require('./server/suggestions')
 const bodyparser=require('body-parser')
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -15,8 +19,11 @@ app.use(bodyparser.json())
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var chat=''
+var id1
+var users={}
 var d=null
 var upload
+var usersdetails={}
 // sessions
 var sess={
     name:'sid',
@@ -34,6 +41,10 @@ var sess={
   app.use('/register',register)
   app.use('/login',login)
   app.use('/logout',logout)
+  app.use('/suggestions',suggestions)
+  app.use('/profile',profile)
+  app.use('/friend',friend)
+  app.use('/messages',messages)
 var store=multer.diskStorage({
 destination:function(req,file,cb){
         cb(null,'./src/assets/uploads1')
@@ -64,8 +75,8 @@ MongoClient.connect(url, function(err, db) {
 io.on('connection',(socket)=>{
      chat=dbo.collection('chat')
     socket.on('join',function(data){
-      
-        socket.join("room");
+        id1=socket.join("room")
+        users[data.mail]=id1
         socket.broadcast.to("room").emit('new user',{name:data.name,message:'joined the room'})
         chat.find().sort({_id:-1}).toArray(function(err,res){
             if(err)
@@ -74,7 +85,7 @@ io.on('connection',(socket)=>{
         })
     })
     socket.on('message',function(data){
-        var c={image:d,name:data.name,message:data.message,likes:0,comment:[],likednames:[],userimage:data.userimage}
+        var c={image:d,name:data.name,message:data.message,likes:0,comment:[],likednames:[],userimage:data.userimage,mail:data.mail}
         // d=null
         dbo.collection("chat").insertOne(c, function(err, res) { 
             if(err) throw err
@@ -131,11 +142,53 @@ socket.on('comment',function(data){
 socket.on('type',function(data){
     socket.broadcast.to("room").emit('typing',data)
 })
+// for personal messages
+socket.on('personalmessage',function(data){
+  data.tomail.length>data.sendermail.length?finalhash=data.tomail+data.sendermail:finalhash=data.sendermail+data.tomail
+  var query={hashvalue:finalhash}
+  dbo.collection('messages').find(query).toArray(function(err,res){
+    if(err)throw err
+    // if already chat exists
+    else if(res.length){
+      result=[]
+     result=res[0].messages
+     result.push({mail:data.sendermail,message:data.message})
+     var newvalues = { $set:{messages:result}};
+     dbo.collection("messages").updateOne(query, newvalues, function(err, res2) {
+         if (err) throw err;
+         // to emit after the chat
+    dbo.collection('messages').find(query).toArray(function(err,response){
+      if(err)throw err
+      // if user in online
+      if(data.tomail in users){
+      users[data.tomail].emit('new_msg',response[0])}
+      users[data.sendermail].emit('new_msg',response[0])
+    })
+     }) }
+    // if chat not exists
+    else{
+      var insertquery={hashvalue:finalhash,messages:[{mail:data.sendermail,message:data.message}]}
+      dbo.collection('messages').insertOne(insertquery,function(err,res){
+        if(err)throw err
+        // to emit after the chat
+    dbo.collection('messages').find(query).toArray(function(err,response){
+      if(err)throw err
+      if(data.tomail in users){
+      users[data.tomail].emit('new_msg',response[0])}
+      users[data.sendermail].emit('new_msg',response[0])
+    })
+      })
+    }
+  })
+})
+// to handle if user left the room
 socket.on('leave', function(data){
-  socket.broadcast.to('room').emit('left room', {name:data.name, message:'has left this room.'});
   socket.leave('room');
+  delete users[data.mail]
 });
-})});
+})
+
+});
 app.post('/image',upload.single('file'),function(req,res){
     res.send(true)
 })
@@ -156,6 +209,10 @@ app.get('/imageget',function(req,res){
     name=req.query['name']
     address=__dirname+'/server/uploads1/'+name
     res.sendFile(address)
+  })
+  // to send user mail
+  app.get('/usermail',function(req,res){
+    res.send(JSON.stringify(req.session.mail))
   })
   // wildcart routing in backend
   app.get('/*',function(req,res){
